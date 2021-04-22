ecbc4e9b9e8ace775cde2ada228294fa3722f7e9
Sergio Alvarez <salvare736@gmail.com>
2021-04-22 12:55:09 -0400

    Title changed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
-      <h1>TESTING CHANGES</h1>
+      <h1>WEB41 RULES</h1>
       <h2>
         Deployed with{' '}
         <a
@@ -42,14 +33,7 @@ function App() {
         <code>/src</code> for components and content, and <code>/api</code>{' '}
         which contains a serverless <a href="https://golang.org/">Go</a>{' '}
         function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
       </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
